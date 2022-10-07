f69a98bb2208ffcf52ebb777afa40a22728c6ecb
Somethea <sometheasiekswx@gmail.com>
2022-10-07 16:43:23 +1100

    Add check-customer-exists

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const cors = require('cors')
 const client = require('@mailchimp/mailchimp_marketing')
+
 require('dotenv').config()
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 client.setConfig({
     apiKey: process.env.MAILCHIMP_API_KEY,
@@ -10,6 +13,46 @@ client.setConfig({
 
 const app = express()
 app.use(cors())
+
+app.post('/api/check-customer-exists', async (req, res) => {
+    const email = req.query.email
+
+    try {
+        const customer = await stripe.customers.list({
+            email: email,
+            limit: 1
+        });
+        if (customer.data.length === 0) res.send(false);
+        if (customer.data[0].email === email) {
+            res.send(true);
+        }
+        res.send(false);
+    } catch (e) {
+        res.status(500, e)
+    }
+});
+
+app.post('/api/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+            {
+                price_data: {
+                    currency: 'aud',
+                    product_data: {
+                        name: 'DevDeakin Premuim by Somethea Siek',
+                    },
+                    unit_amount: 99,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: 'payment',
+        success_url: `${process.env.REACT_FRONTEND_BASE_URL}/plans`,
+        cancel_url: `${process.env.REACT_FRONTEND_BASE_URL}/plans`,
+    });
+
+    res.redirect(303, session.url);
+});
 
 app.post('/api/subscribe', async (req, res) => {
     const email = req.query.email
